2368c4c0fb6fee1c8a4169096a8c55d9cdf30265
SND\thomaskluiters_cp <SND\thomaskluiters_cp@aadf2aa7-86a3-4242-a263-039aa7a476c3>
2015-02-24 00:05:33 +0000

    Added documentation to Utilities.

--- Front-end/Utilities.fs
+++ Front-end/Utilities.fs
@@ -3,15 +3,25 @@
 let debug_log (x:'a) = 
   do printfn "%A" x
   x
-
+(*
+    The Either type is a simple data structure used to hold two types 'a and 'b.
+*)
 type Either<'a,'b> = First of 'a | Second of 'b
   with 
+    (*
+        The function Fold is a helper function to apply functions to Either's First and Second member.
+        If this Either has a First element then apply to given function fo First, else Apply the given function g to Second.
+    *)
     member e.Fold f g =
             match e with
             | First x -> f x
             | Second y -> g y
 
 
+
+(*
+    The matching_brackets function is a helper function to test for bracket equality between two open and closing brackets.
+*)
 let matching_brackets open_bracket closed_bracket = 
   match open_bracket, closed_bracket with
   | '(', ')' -> true
