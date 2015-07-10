582385060aaa3b050c6661d41ba84cb5d3e41122
snipstruis <kipstad@hotmail.com>
2015-07-10 13:11:57 +0200

    fixed fix

--- Front-end/CodeGenerator.fs
+++ Front-end/CodeGenerator.fs
@@ -8,7 +8,6 @@ open BasicExpression
 open TypeDefinition
 open ConcreteExpressionParserPrelude
 open ConcreteExpressionParser
-open CodeGeneratorDeps
 
 type TypedExpression   = BasicExpression<Keyword, Var, Literal, Position, Type>
 type UntypedExpression = BasicExpression<Keyword, Var, Literal, Position, unit>
--- Front-end/CompilerSwitches.fs
+++ Front-end/CompilerSwitches.fs
@@ -6,6 +6,8 @@ let printExpressionTree = false
 let flushCSFileOnError = false
 let generateLineDirectives = false
 
+let useGraphBasedCodegen = false
+
 let optimizeDirectParentCall = true
 let generateStaticRun = true
 let combineCreateFor  = true
