e076e2bca2f8e5d0b3c49dcbdfc6c77a26578d41
snipstruis <kipstad@hotmail.com>
2015-07-10 13:48:22 +0200

    added graph based code generator

---
+++ Front-end/GraphBasedCodeGenerator.fs
@@ -0,0 +1,20 @@
+﻿module GraphBasedCodeGenerator
+
+open TypeInference
+open System
+open Utilities
+open ParserMonad
+open BasicExpression
+open TypeDefinition
+open ConcreteExpressionParserPrelude
+open ConcreteExpressionParser
+
+type TypedExpression   = BasicExpression<Keyword, Var, Literal, Position, Type>
+type UntypedExpression = BasicExpression<Keyword, Var, Literal, Position, unit>
+
+let generate (originalFilePath:string)
+             (program_name:string)
+             (rules:UntypedExpression) // <-- ast
+             (program:UntypedExpression) // <-- main
+             (ctxt:ConcreteExpressionContext) // <-- header
+             :unit = ()
