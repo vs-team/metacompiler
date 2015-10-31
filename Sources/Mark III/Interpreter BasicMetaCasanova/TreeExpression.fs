0b01b919c79a279cac7c088603ed62223906b8c9
snipstruis <kipstad@hotmail.com>
2015-10-31 23:53:00 +0100

    can now add the implicit parentheses to expressions (does not look inside parens yet)

--- Sources/Mark III/Interpreter BasicMetaCasanova/TreeExpression.fs
+++ Sources/Mark III/Interpreter BasicMetaCasanova/TreeExpression.fs
@@ -9,6 +9,9 @@ let pivot (f:'a -> bool) (lst:List<'a>) :List<'a>*'a*List<'a> =
   let left,pivot::right = lst |> List.splitAt (lst |> List.findIndex f)
   left,pivot,right
 
+let rec takeBack (n:int) (lst:List<'a>) : List<'a> =
+  if List.length lst <= n then lst else takeBack n lst.Tail
+
 // I am unhappy about this
 let rec tryExtractPosition(expr:BasicExpression) :Option<Position> =
   match expr with
@@ -49,31 +52,52 @@ let pivotPosition pos exprs =
     | Id(sym,found_pos) -> pos=found_pos 
     | _ -> false)
 
-let rec treeify (scope:Scope) (exprs:List<BasicExpression>) :Option<TreeExpression> =
-  match exprs with
-  | [BasicExpression.Literal (l,pos)] -> Some (Literal (l,pos))
-  | exprs  -> 
-    let ops = exprs |> extractOperators scope
-    if ops.IsEmpty then
-      // very unhappy indeed
-      match tryAny tryExtractPosition exprs with
-      | Some pos -> do printfn "Error in %s on line %d col %d: No operator in subexpression." pos.File pos.Line pos.Col
-                    None
-      | None     -> do printfn "Error somewhere idk, maybe in a file or something?: No operator in subexpression."
-                    None
-    else
-      let operatorSymbol,operatorPosition = ops |> getHighestPriorityOperator
-      let leftArgs,_,rightArgs = exprs |> pivotPosition operatorPosition
-      if operatorSymbol.LeftArgs.Length < leftArgs.Length then
-        do printfn "Error in %s on line %d col %d: Left argument(s) expected." operatorPosition.File operatorPosition.Line operatorPosition.Col
-        do printfn "  Operator %s expects %d arguments to its left, but received %d." operatorSymbol.Name operatorSymbol.LeftArgs.Length leftArgs.Length
-        do printfn "  Add missing operand(s) to its left."
-        None
-      elif operatorSymbol.RightArgs.Length > rightArgs.Length then
-        do printfn "Error in %s on line %d col %d: Right argument(s) expected." operatorPosition.File operatorPosition.Line operatorPosition.Col
-        do printfn "  Operator %s expects %d arguments to its right, but received %d." operatorSymbol.Name operatorSymbol.RightArgs.Length rightArgs.Length
-        do printfn "  Add missing operand(s) to its right."
-        None
-      else 
-        // TODO: finish
-        None
+let getPriorityOperator (acc:SymbolDeclaration,acc_pos:Position) (right:SymbolDeclaration,right_pos:Position) :SymbolDeclaration*Position =
+  if   acc.Priority > right.Priority then acc,acc_pos
+  elif acc.Priority < right.Priority then right,right_pos
+  elif acc.Associativity = right.Associativity then
+    do printfn "Warning in %s on line %d col %d: Ambiguous order of operations." acc_pos.File acc_pos.Line acc_pos.Col
+    do printfn "  Operators %s and %s have the same priority, but different associativity." acc.Name right.Name
+    do printfn "  Add parentheses to disambiguate. (will continue with left associativity)"
+    acc,acc_pos
+  elif acc.Associativity = Left then acc,acc_pos
+  else right,right_pos 
+
+let getOperator scope expr =
+  match expr with
+  | Id(str,pos) -> let symbol = findSymbol str scope
+                   if symbol.LeftArgs.IsEmpty && symbol.RightArgs.IsEmpty 
+                   then None else Some (symbol,pos) 
+  | _ -> None
+
+let compare scope (acc:SymbolDeclaration*Position) (expr:BasicExpression) =
+  match getOperator scope expr with
+  | None   -> acc
+  | Some x -> getPriorityOperator acc x
+
+// todo: also look inside parens to see if non-parenthesized expressions lurk there
+let rec addParens (scope:Scope) (exprs:List<BasicExpression>) :List<BasicExpression> =
+  let fromFirstId = exprs |> List.skipWhile ((getOperator scope)>>Option.isSome)
+  if fromFirstId.IsEmpty then exprs
+  else 
+    let operatorSymbol,operatorPosition = 
+      fromFirstId |> List.fold (compare scope) (Option.get (getOperator scope fromFirstId.Head))
+    let left,_,right = pivotPosition operatorPosition exprs 
+    let leftArgs     = left  |> takeBack  operatorSymbol.LeftArgs.Length
+    let rightArgs    = right |> List.take operatorSymbol.RightArgs.Length
+    if operatorSymbol.LeftArgs.Length < leftArgs.Length then
+      do printfn "Error in %s on line %d col %d: Left argument(s) expected." operatorPosition.File operatorPosition.Line operatorPosition.Col
+      do printfn "  Operator %s expects %d arguments to its left, but received %d." operatorSymbol.Name operatorSymbol.LeftArgs.Length leftArgs.Length
+      do printfn "  Add missing operand(s) to its left."
+      []
+    elif operatorSymbol.RightArgs.Length > rightArgs.Length then
+      do printfn "Error in %s on line %d col %d: Right argument(s) expected." operatorPosition.File operatorPosition.Line operatorPosition.Col
+      do printfn "  Operator %s expects %d arguments to its right, but received %d." operatorSymbol.Name operatorSymbol.RightArgs.Length rightArgs.Length
+      do printfn "  Add missing operand(s) to its right."
+      []
+    else 
+      let farLeft  = left  |> List.take (left.Length-leftArgs.Length)
+      let farRight = right |> takeBack  (right.Length-rightArgs.Length)
+      let newExpr = Id(operatorSymbol.Name,operatorPosition)
+      let newParen= Application(Round,leftArgs@(newExpr::rightArgs))
+      addParens scope farLeft@(newParen::farRight)
