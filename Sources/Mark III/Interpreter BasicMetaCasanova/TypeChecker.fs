d8b40ddd934740506cb77dfa668ee81cdd2c0872
snipstruis <kipstad@hotmail.com>
2015-11-16 01:26:25 +0100

    working on prioritizer...

--- Sources/Mark III/Interpreter BasicMetaCasanova/TypeChecker.fs
+++ Sources/Mark III/Interpreter BasicMetaCasanova/TypeChecker.fs
@@ -61,7 +61,6 @@ and MaybeType = Conflict of List<TreeExpr*TreeExpr>
 
 type Expr = Basic of BasicExpression | Tree of TreeExpr
 
-// Question: does this work correctly with shadowed identifiers?
 let rec selectDecls (exprs:List<BasicExpression>) (decls:List<SymbolDeclaration>) : List<SymbolDeclaration> =
   // recursively find names of operators and add them to the set
   let rec used_names exprs decls =
@@ -71,15 +70,43 @@ let rec selectDecls (exprs:List<BasicExpression>) (decls:List<SymbolDeclaration>
       | Application(_,e) -> acc + (used_names e decls)
       | _ -> acc) Set.empty
   let set = used_names exprs decls
-  // sort the set into a list according to the order in decls
-  let lst = decls |> List.fold (fun acc next -> if set.Contains(next.Name) then next::acc else acc) []
-  List.rev lst
+  // sort them on priority
+  decls |> List.filter   (fun x-> set.Contains(x.Name))
+        |> List.sortWith (fun x y-> x.Priority - y.Priority)
 
-let prioritize (exprs:List<BasicExpression>) (decls:List<SymbolDeclaration>) :List<Expr> =
-   let decls = selectDecls exprs decls
-   let exprs = exprs |> List.map Basic
-   let rec prioritize (exprs:List<Expr>) = []
-   prioritize exprs
+let capture (exprs:List<Expr>) (decls:List<SymbolDeclaration>) :List<Expr> = 
+  let foo = decls |> List.map (fun decl-> 
+    let idx = match decl.Associativity with 
+              | Left  -> exprs |> List.tryFindIndex     (fun x->"todo"=decl.Name)
+              | Right -> exprs |> List.tryFindIndexBack (fun x->"todo"=decl.Name)
+    idx)
+  []
+
+let rec prioritize (exprs:List<BasicExpression>) (decls:List<SymbolDeclaration>) :Option<TreeExpr> =
+  // first apply this function recursively to parens
+  let decls = selectDecls exprs decls
+  let exprs = exprs |> List.map (fun expr ->
+    match expr with
+    | Application(b,e) -> prioritize exprs decls |> Option.map (Tree)
+    | Literal(l,p)     -> match l:Literal with
+                          | Int     _ -> Some(Tree(Lit(l,TypeId("int"),p)))
+                          | String  _ -> Some(Tree(Lit(l,TypeId("string"),p)))
+                          | Float32 _ -> Some(Tree(Lit(l,TypeId("float"),p)))
+    | Id(s,p) when (decls|>List.exists(fun x->x.Name=s)|>not) -> Some(Tree(Var(s,Unknown,p)))
+    | Id(s,p)          -> Some(Basic(Id(s,p)))
+    | Lamda(_)        -> do printfn "can't deal with lambdas."
+                         None // TODO
+    | Scope(_)         -> do printfn "can't deal with scopes."
+                          None
+    | Arrow(_,_)       -> do printfn "can't deal with arrows."
+                          None )// what even are arrows?
+  if exprs |> List.contains None then None else
+  let exprs = exprs |> List.map Option.get
+  // then capture the parameters of the functions as declared in decls
+  match capture exprs decls with
+  | [Tree x] -> Some(x)
+  | _ -> do printfn "capturing failed."
+         None
 
 let listToMapOfLists (lst:List<'k*'v>) :Map<'k,List<'v>> =
   lst |> List.fold 
