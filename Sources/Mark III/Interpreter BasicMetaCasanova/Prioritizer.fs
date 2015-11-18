682e7381ac23e25c2d67f40fecdb5062b22efd1e
Jarno Holstein <mj.holstein@hotmail.com>
2015-11-18 13:36:36 +0100

    begin monad for typechecker.

--- Sources/Mark III/Interpreter BasicMetaCasanova/Prioritizer.fs
+++ Sources/Mark III/Interpreter BasicMetaCasanova/Prioritizer.fs
@@ -2,4 +2,28 @@
 open Common
 open ScopeBuilder
 
+type Result<'scope,'typescope,'result> = 
+  | Done of 'result * 'scope * 'typescope
+  | Error of string * Position
+
+type TypeParser<'scope,'typescope,'result> = 
+  'scope * 'typescope -> Result<'scope,'typescope,'result>
+
+type TypeParserBuilder() =
+  member this.Return(res:'result) : TypeParser<'scope,'typescope,'result> =
+    fun (scope,typescope) ->
+      Done(res,scope,typescope)
+  member this.ReturnFrom p = p
+  member this.Bind(p:TypeParser<'scope,'typescope,'result>, k:'result->TypeParser<'scope,'typescope,'result'>) =
+    fun (scope,typescope) ->
+      match p (scope,typescope) with
+      | Error(e,p) -> Error(e,p)
+      | Done(res,scope',typescope') ->
+        let out = k res (scope',typescope')
+        out
+
+let typ = TypeParserBuilder()
+
+
+
 (* -- see TypeChecker.fs -- *)
