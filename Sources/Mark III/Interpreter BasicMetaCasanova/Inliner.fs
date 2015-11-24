5ba29f1067f44c31596135da5d69bd5b7c33d794
snipstruis <kipstad@hotmail.com>
2015-11-24 18:17:52 +0100

    started work on inliner

--- Sources/Mark III/Interpreter BasicMetaCasanova/Inliner.fs
+++ Sources/Mark III/Interpreter BasicMetaCasanova/Inliner.fs
@@ -1,3 +1,22 @@
 ﻿module Inliner
+open Common
+open ScopeBuilder
+open TypeChecker
 
-(* comming soon! *)
+// eventually needs an evaluator, for now: special-case the Signatures and
+// push everything else to run-time
+
+type ConcreteType = Int8 | Int16 | Int32 | Int64 | Float | Double | String
+                  | Struct    of List<ConcreteType>
+                  | Reference of ConcreteType
+                  | Function  of ConcreteType*List<ConcreteType>
+                  | List      of ConcreteType
+type TemplatedType = List<ConcreteType>
+
+let FindSignatures (decls:Map<Id,SymbolDeclaration*Type>) =
+  let rec hasSignature k (s,t) =
+    match t with
+    | Signature -> true
+    | BigArrow (_,r) -> hasSignature k (s,r)
+    | _ -> false
+  decls |> Map.filter hasSignature 
