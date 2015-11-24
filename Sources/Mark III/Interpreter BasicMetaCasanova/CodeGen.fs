a54ff9ef8b574bcd61f92221e64d6ab461c72b25
snipstruis <kipstad@hotmail.com>
2015-11-24 21:13:13 +0100

    added mangler

--- Sources/Mark III/Interpreter BasicMetaCasanova/CodeGen.fs
+++ Sources/Mark III/Interpreter BasicMetaCasanova/CodeGen.fs
@@ -1,3 +1,12 @@
 ﻿module CodeGen
 
-(* comming soon! *)
+let mangle (name:string) :string =
+  let normalize (name:string) :string =
+    let bytes = System.Text.Encoding.Default.GetBytes(name)
+    let str   = System.Text.Encoding.UTF8.GetString(bytes)
+    System.String.Intern(str).Normalize(System.Text.NormalizationForm.FormKC)
+  let mangleChar c =
+    if (c>='a'&&c<='z') || (c>='A'&&c<='Z') || (c>='0'&&c<='9')
+    then sprintf "%c" c
+    else sprintf "_%02X" (System.Convert.ToByte(c))
+  name |> normalize |> String.collect mangleChar
