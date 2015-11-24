14583e14c92faad70b6382d4a48b41f9e4c2746b
snipstruis <kipstad@hotmail.com>
2015-11-24 18:39:07 +0100

    we need some sort of InlinedScope, also String is not a primitive

--- Sources/Mark III/Interpreter BasicMetaCasanova/Inliner.fs
+++ Sources/Mark III/Interpreter BasicMetaCasanova/Inliner.fs
@@ -6,12 +6,18 @@ open TypeChecker
 // eventually needs an evaluator, for now: special-case the Signatures and
 // push everything else to run-time
 
-type ConcreteType = Int8 | Int16 | Int32 | Int64 | Float | Double | String
+type ConcreteType = Int8 | Int16 | Int32 | Int64 | Float | Double
                   | Struct    of List<ConcreteType>
                   | Reference of ConcreteType
                   | Function  of ConcreteType*List<ConcreteType>
                   | List      of ConcreteType
 type TemplatedType = List<ConcreteType>
+
+type InlinedScope = {
+  Parents   : List<Id*InlinedScope>
+  DataDecls : Map<Id,SymbolDeclaration*Type>
+  FuncRules : Map<Id,List<Rule>>
+}
 
 let FindSignatures (decls:Map<Id,SymbolDeclaration*Type>) =
   let rec hasSignature k (s,t) =
