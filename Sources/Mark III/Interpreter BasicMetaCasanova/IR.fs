77aff22b5c2d8d39e39ac7456b3db199d38eabac
snipstruis <kipstad@hotmail.com>
2016-03-02 11:39:28 +0100

    updated length ast-program

--- Sources/Mark III/Interpreter BasicMetaCasanova/IR.fs
+++ Sources/Mark III/Interpreter BasicMetaCasanova/IR.fs
@@ -59,43 +59,48 @@ let print_local_id n = match n with Named x -> CSharpMangle x | Tmp x -> sprintf
 
 
 type NamespacedItem = Ns     of string*List<NamespacedItem>
-                    | Data   of string*data
-                    | Func   of string*List<rule>
-                    | Lambda of int*rule
+                    | Data   of string*Type*data
+                    | Func   of string*Type*List<rule>
+                    | Lambda of int*Type*rule
 
 let construct_tree (input:fromTypecheckerWithLove) :List<NamespacedItem> =
-  let rec datatree (s:List<NamespacedItem>) (idx:List<string>,v:string*data) :List<NamespacedItem> =
+  let rec datatree (s:List<NamespacedItem>) (idx:List<string>,v:string*Type*data) :List<NamespacedItem> =
     match idx with
     | []    -> (Data(v))::s
     | n::ns -> match s |> List.partition (fun x->match x with Ns(n,_)->true | _->false) with
                | [Ns(n,body)],rest -> Ns(n,datatree body (ns,v))::rest
                | [],list           -> Ns(n,datatree []   (ns,v))::list
-  let rec functree (s:List<NamespacedItem>) (idx:List<string>,v:string*List<rule>) :List<NamespacedItem> =
+  let rec functree (s:List<NamespacedItem>) (idx:List<string>,v:string*Type*List<rule>) :List<NamespacedItem> =
     match idx with
     | []    -> (Func(v))::s
     | n::ns -> match s |> List.partition (fun x->match x with Ns(n,_)->true | _->false) with
                | [Ns(n,body)],rest -> Ns(n,functree body (ns,v))::rest
                | [],list           -> Ns(n,functree []   (ns,v))::list
-  let rec lambdatree (s:List<NamespacedItem>) (idx:List<string>,v:int*rule) :List<NamespacedItem> =
+  let rec lambdatree (s:List<NamespacedItem>) (idx:List<string>,v:int*Type*rule) :List<NamespacedItem> =
     match idx with
     | []    -> (Lambda(v))::s
     | n::ns -> match s |> List.partition (fun x->match x with Ns(n,_)->true | _->false) with
                | [Ns(n,body)],rest -> Ns(n,lambdatree body (ns,v))::rest
                | [],list           -> Ns(n,lambdatree []   (ns,v))::list
-  let go input output state = input |> Map.toSeq |> Seq.map (fun (n,d)->n.Namespace,(n.Name,d)) |> Seq.fold output state
+  let go input output state = input |> Map.toSeq |> Seq.map (fun (n,d)->n.Namespace,(n.Name,n.Type,d)) |> Seq.fold output state
   [] |> go input.lambdas lambdatree |> go input.rules   functree |> go input.datas datatree
           
 let rec print_tree (ns:List<NamespacedItem>) :string =
   let print_base_types (ns:List<NamespacedItem>) = 
-    let types = ns |> List.fold (fun types item -> match item with Data (_,v) -> v.output::types | _ -> types) [] |> List.distinct
+    let types = ns |> List.fold (fun types item -> match item with Data (_,_,v) -> v.output::types | _ -> types) [] |> List.distinct
     let print t = sprintf "public class %s{}\n" (t|>strip_namespace_type|>mangle_type_suffix)
     List.map print types
   let field (n:local_id,t:Type) = sprintf "public %s %s;\n" (mangle_type t) (print_local_id n)
+  let print_func (s:string) (t:Type) (rules:List<rule>) =
+    let static_call  = sprintf "public static %s _call(%s){%s}" "RET_TYPE" "ARGS" "BODY"
+    let closure_call = sprintf "public %s _closure_call(){return call(%s);}" "RET_TYPE" "ARGS"
+    let body = "BODY"
+    sprintf "public class %s{\n%s}" (CSharpMangle s)
   let go ns = match ns with
               | Ns (n,ns) -> sprintf "namespace %s{\n%s}\n" (CSharpMangle n) (print_tree ns)
-              | Data (n,d) -> sprintf "public class %s:%s{\n%s}\n" (CSharpMangle n) (d.output|>strip_namespace_type|>mangle_type_suffix) (d.args|>List.map field|>String.concat "")
-              | Func (s,_) -> sprintf "// todo: Func %s\n" (CSharpMangle s)
-              | Lambda (s,_) -> sprintf "// todo: Lambda nr %d\n" s
+              | Data (n,_,d) -> sprintf "public class %s:%s{\n%s}\n" (CSharpMangle n) (d.output|>strip_namespace_type|>mangle_type_suffix) (d.args|>List.map field|>String.concat "")
+              | Func (s,_,_) -> sprintf "// todo: Func %s\n" (CSharpMangle s)
+              | Lambda (s,_,_) -> sprintf "// todo: Lambda nr %d\n" s
   (print_base_types ns)@(ns|>List.map go)|>String.concat "\n"
     
 let failsafe_codegen(input:fromTypecheckerWithLove) =
@@ -129,42 +134,56 @@ let test_data:fromTypecheckerWithLove=
 
 let list_test:fromTypecheckerWithLove =
   let int_t:Type   = DotNetType(["System"],"Int32");
+  let add_t:Type = Arrow(int_t,Arrow(int_t,int_t))
+  let add_id:Id  = {Namespace=["builtin"];Name="add";Type=add_t}
+
+  // data "nil" -> List int
   let list_t:Type  = TypeApplication((McType(["mc";"test"],"List")),[int_t])
-  let append_id:Id = {Namespace=["mc";"test"];Name="::";Type=list_t}
   let nil_id:Id    = {Namespace=["mc";"test"];Name="nil";Type=list_t}
-  let append_data:data =
-    {
-      args=[Named("x"),int_t; Named("xs"),int_t];
-      output=list_t;
-    }
   let nil_data:data =
     {
       args=[];
       output=list_t;
     }
 
-  let add_t:Type = Arrow(int_t,Arrow(int_t,int_t))
-  let add_id:Id  = {Namespace=["builtin"];Name="add";Type=add_t}
-
+  // data x -> "::" -> xs -> List int
+  let append_id:Id = {Namespace=["mc";"test"];Name="::";Type=list_t}
+  let append_data:data =
+    {
+      args=[Named("x"),int_t; Named("xs"),int_t];
+      output=list_t;
+    }
+  
+  // length nil -> 0
   let length_t:Type= Arrow (list_t,int_t)
   let length_id:Id = {Namespace=["mc";"test"];Name="length";Type=length_t}
   let length_nil:rule =
     {
-      input=[DestructorCall(nil_id,[])]
-      premis=[]
-      output=Lit(I32(0))
-      typemap=Map.empty
+      input=[Tmp(0)]
+      premis=[DestructorCall(Tmp(0),nil_id,[]);
+              Literal(Tmp(1),lit.I32(0))]
+      output=Tmp(1)
+      typemap=Map.ofSeq [Tmp(0),list_t
+                         Tmp(1),int_t]
     }
+
+  // length xs -> r
+  // -------------------------------
+  // length x::xs -> add^builtin r 0
   let length_append:rule =
     {
-      input=[DestructorCall(append_id,[Named("x");Named("xs")])]
-      premis=[Assignment(RuleCall(global_id.Named(length_id),[Named("xs")]),Id(Named("r")))
-              Assignment(Lit(I32(1)),Id(Tmp(0)))]
-      output=DotNetCall(global_id.Named(add_id),[Named("r");Tmp(0)])
-      typemap=Map.ofSeq [Named("x"),int_t;
-                         Named("xs"),list_t;
-                         Named("r"),int_t;
-                         Tmp(0),int_t]
+      input=[Tmp(0)]
+      premis=[DestructorCall(Tmp(0),append_id,[Named("x");Named("xs")])
+              Call(Named("r"),global_id.Named(length_id),[Named("xs")])
+              Literal(Named("r"), lit.I32(0))
+              BuiltinCall(Tmp(2),ADD,[Named("r");Tmp(1)])]
+      output=Tmp(2)
+      typemap=Map.ofSeq [Named("x"),int_t
+                         Named("xs"),list_t
+                         Named("r"),int_t
+                         Tmp(0),list_t
+                         Tmp(1),int_t
+                         Tmp(2),int_t]
     }
 
   let datas = Map.ofSeq <| [nil_id,nil_data; append_id,append_data]
