02e3eefd75e2d8229f09fbd2faf8e4418d0a59d6
Jarno Holstein <mj.holstein@hotmail.com>
2016-03-04 15:13:12 +0100

    added RuleNormalizer and RuleTypeChecker.

---
+++ Sources/Mark III/Interpreter BasicMetaCasanova/RuleNormalizer2.fs
@@ -0,0 +1,16 @@
+﻿module RuleNormalizer2
+
+open Common
+open RuleParser2
+
+
+
+type NormalizedRule =
+  {
+    Input : List<Id>
+    Output : Id
+    Premis : List<Id>
+    Typemap : List<Id>
+    
+  }
+
