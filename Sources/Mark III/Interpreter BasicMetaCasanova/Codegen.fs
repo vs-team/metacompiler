34cc4cc39378c7449df06cc56922d848fff4d6cf
snipstruis <kipstad@hotmail.com>
2016-03-10 12:03:22 +0100

    saving progress

--- Sources/Mark III/Interpreter BasicMetaCasanova/Codegen.fs
+++ Sources/Mark III/Interpreter BasicMetaCasanova/Codegen.fs
@@ -49,13 +49,6 @@ let rec mangle_type(t:Type):string=
   | McType     (id) -> id.Namespace@[id.Name] |> Seq.map (fun x->if x="System" then "_System" else CSharpMangle x) |> String.concat "."
   | TypeApplication (fn,lst) -> mangle_type fn
 
- (*
-let rec strip_namespace_type(t:Type) :Type = 
-  match t with
-  | DotNetType (id) -> DotNetType ([],id.Name)
-  | McType     (_,n) -> McType     ([],n)
-  | TypeApplication (fn,lst) -> TypeApplication ((strip_namespace_type fn),lst)
-  *)
 
 let print_local_id n = match n with Named x -> CSharpMangle x | Tmp x -> sprintf "_tmp%d" x 
 
@@ -63,8 +56,6 @@ type NamespacedItem = Ns       of string*List<NamespacedItem>
                     | Data     of string*data
                     | Function of string*List<rule>
                     | Lambda   of int*rule
-
-
 
 let construct_tree (input:fromTypecheckerWithLove) :List<NamespacedItem> =
   let rec datatree (s:List<NamespacedItem>) (idx:List<string>,v:string*data) :List<NamespacedItem> =
@@ -98,94 +89,28 @@ let print_literal lit =
   | String s -> sprintf "\"%s\"" s
   | Bool b   -> if b then "true" else "false"
 
-  (*
-let print_global_id id = "ID"
-  //match id with
-  //| global_id.Lambda l -> sprintf "%s.%s" (String.concat l.Namespace ".") l.Name
-
-let rec func_arity (t:Type) :int =
-  match t with
-  | Arrow (_,x) -> 1 + (func_arity x)
-  | _ -> 0
-
-
-let rec print_rule(r:rule)(t:Map<string,Type>):string =
-  match p with
-  | [] -> ""
-  | p::ps ->
-    match p with
-    | Literal(l,id) -> sprintf "val %s = %s;\n%s" (print_local_id id) (print_literal l) (print_premises p highest_tmp)
-    | Conditional(x,c,y) -> 
-      let cond = 
-        match c with
-        | Less -> "<"
-        | LessEqual -> "<="
-        | Equal -> "="
-        | GreaterEqual -> ">="
-        | Greater -> ">"
-        | NotEqual -> "!="
-      sprintf "if(%s%s%s){%s}" (print_local_id x) cond (print_local_id y) (print_premises ps highest_tmp)
-    | Destructor(from,id,ret) ->
-      // var $tmp = $from as $specific_type
-      // if($tmp!=null){
-      //   var %arg[i] = %tmp.%arg[i]
-      let new_highest_tmp = highest_tmp+1
-      let new_tmp = print_local_id (Tmp(new_tmp))
-      let cast = sprintf "var %s = %s as %s;" new_tmp (print_local_id from) (print_global id)
-      let args = ret |> List.map (fun x-> sprintf "var %s=%s.%s;")
-      let cond = sprintf "if(%s!=null){%s%s}" new_tmp 
-      
-      
-
-let print_func (s:string) (t:Type) (rules:List<rule>) =
-  //let print_premisse (p:premisse) =
-  //  match p with
-  //  | Literal (loc,lit)    -> sprintf "var %s=%s;\n" (print_local_id loc) (print_literal lit)
-  //  | Call (loc,func,args) -> sprintf "var %s=%s" (print_local_id loc) (print_global_id func)
-  let print_rule = "{\nrule\n}\n" 
-  let body = sprintf "List<%s> _ret;\n%s\nreturn _ret;\n" "RET_TYPE" "BODY"
-  let print_func = sprintf "public static List<%s> _call(%s){%s}" "RET_TYPE" "RULES" body
-  let closure_call = sprintf "public %s _closure_call(){return call(%s);}" "RET_TYPE" "ARGS"
-  sprintf "public class %s{\n%s}" (CSharpMangle s) body
-*)
-
 let field (n:local_id,t:Type) = sprintf "public %s %s;\n" (mangle_type t) (print_local_id n)
 
-let get_locals (ps:premisse list) :local_id list =
-  ps |> List.collect (fun p ->
-    match p with
-    | Literal             x -> [x.dest]
-    | Conditional         x -> [x.left;x.right]
-    | Destructor          x -> x.source::x.args
-    | McClosure           x -> [x.dest]
-    | DotNetClosure       x -> [x.dest]
-    | ConstructorClosure  x -> [x.dest]
-    | Application         x -> [x.closure;x.dest;x.argument] ) |> List.distinct
+let highest_tmp (typemap:Map<local_id,Type>): int =
+  typemap |> Map.fold (fun s k _ -> match k with Tmp(x) when x>s -> x | _ -> s) 0
 
-let highest_tmp (ps:premisse list) :int =
-  ps |> get_locals |> List.fold 
-    (fun s x-> match x with Tmp(x)->x::s | _ -> s) []
-    |> List.max
-    (*
-let print_rule (r:rule) (lookup:fromTypecheckerWithLove) =
-  let max_tmp = highest_tmp r.premis
-  let prem (p:premisse) (ps:premisse list) (max_tmp:int) =
-    match p with
-    | Destructor (from,type_id,args) ->
-      let max_tmp = max_tmp+1
-      let data = lookup.data.[type_id].output |> strip_namespace_type |>
-      let cast_line = sprintf "var %s = %s as %s;\n" (Tmp(max_tmp))
-      *)
 let rec print_tree (lookup:fromTypecheckerWithLove) (ns:List<NamespacedItem>) :string =
+  let build_func (name:string) (rules:rule list) = 
+    let rule = List.head rules
+    let args = "/*ARGS*/" //rule.input |> Seq.map (fun id-> rule.typemap.[id])
+    let ret_type = "/*RET*/"
+    let rules = "/*RULES*/"
+    sprintf "class %s{\n%s\npublic List<%s> run(){\nList<%s> _ret;\n%s\nreturn _ret;\n}\n}\n" (CSharpMangle name) args ret_type ret_type rules
   let print_base_types (ns:List<NamespacedItem>) = 
     let types = ns |> List.fold (fun types item -> match item with Data (_,v) -> v.outputType::types | _ -> types) [] |> List.distinct
     let print t = sprintf "public class %s{}\n" (t|>mangle_type_suffix)
     List.map print types
-  let go ns = match ns with
-              | Ns(n,ns)     -> sprintf "namespace %s{\n%s}\n" (CSharpMangle n) (print_tree lookup ns)
-              | Data(n,d)    -> sprintf "public class %s:%s{\n%s}\n" (CSharpMangle n) (d.outputType|>mangle_type_suffix) (d.args|>List.map field|>String.concat "")
-              | Function(s,_)-> sprintf "// todo: Func %s\n" (CSharpMangle s)
-              | Lambda(s,_)  -> sprintf "// todo: Lambda nr %d\n" s
+  let go ns =
+    match ns with
+    | Ns(n,ns)      -> sprintf "namespace %s{\n%s}\n" (CSharpMangle n) (print_tree lookup ns)
+    | Data(n,d)     -> sprintf "public class %s:%s{\n%s}\n" (CSharpMangle n) (d.outputType|>mangle_type_suffix) (d.args|>List.map field|>String.concat "")
+    | Function(name,rules) -> build_func name rules
+    | Lambda(number,rule)  -> build_func (sprintf "_L%d" number) [rule]
   (print_base_types ns)@(ns|>List.map go)|>String.concat "\n"
     
 let failsafe_codegen(input:fromTypecheckerWithLove) =
